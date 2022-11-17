2e1b810dfcf04bcfe8262d3ea502c40b16ca830b
Bill3710 <ljting207@gmail.com>
2022-11-16 23:43:19 -0800

    Update Utils.java

--- src/main/java/citynetwork/Utils.java
+++ src/main/java/citynetwork/Utils.java
@@ -7,11 +7,11 @@ public class Utils {
 
     public static boolean isValidCode(String _cityName, String _cityCode) {
         int codeIdx = 0, cityIdx = 0;
-        boolean valid = true;
+
 
         List<Character> cityChar = new ArrayList<>();
         List<Character> cityCodeChar = new ArrayList<>();
-        List<Integer> cityCodeIdx = new ArrayList<>();
+
 
         if(_cityCode.contains(" ")){return false;}
         for(int i=0;i<_cityName.length();i++){
@@ -30,6 +30,7 @@ public class Utils {
         if(codeIdx!=2){
             return false;
         }
+
         return true;
     }
 
